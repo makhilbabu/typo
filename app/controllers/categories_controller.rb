fb1f1c5474fa7b6cd83ca8ad0d2fc74448d48df3
makhilbabu <manamakhilbabu@gmail.com>
2018-04-03 22:25:51 +0000

    App working properly

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,6 @@ class CategoriesController < GroupingController
   # show - inherited
   
   def index
-    @category = Category.all
   end
   
   def show
--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -15,6 +15,25 @@ describe Admin::CategoriesController do
     get :index
     assert_response :redirect, :action => 'index'
   end
+  
+  describe "test_create" do
+    before(:each) do
+      get :new, :id => Factory(:category).id
+    end
+
+    it 'should render template new' do
+      assert_template 'new'
+      assert_tag :tag => "table",
+        :attributes => { :id => "category_container" }
+    end
+
+    it 'should create a new category' do
+      post :edit, :category => {:name => "Foobar", :keywords => "Lorem Ipsum", :permalink => "permalinking", :description => "descripting"}
+      assert_response :redirect, :action => "index"
+      expect(assigns(:category)).not_to be_nil
+      expect(flash[:notice]).to eq('Category was successfully saved.')
+    end
+  end
 
   describe "test_edit" do
     before(:each) do
@@ -48,7 +67,7 @@ describe Admin::CategoriesController do
 
     it 'should render destroy template' do
       assert_response :success
-      assert_template 'destroy'      
+      assert_template 'destroy'
     end
   end
 
